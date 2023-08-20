22542a75b3801da3415d12505710b1eb659e4b33
abdenassar <abdenassaramimi99@gmail.com>
2023-08-20 15:08:16 +0100

    delete boilerplate + adding styled comp

---
+++ app/tabs/tab-navigator/tab-navigator.tsx
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const TabNavigator = () => {
+  return (
+    <View>
+      <Text>tab-navigator</Text>
+    </View>
+  );
+};
+
+export default TabNavigator;
