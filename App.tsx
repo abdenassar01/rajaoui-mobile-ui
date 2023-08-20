ba4ba207f6dd3482802ab6293009e3739691904b
abdenassar <abdenassaramimi99@gmail.com>
2023-08-20 15:18:41 +0100

    adding react navigations and setup first tab

--- App.tsx
+++ App.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import {SafeAreaView, StatusBar} from 'react-native';
 import TabNavigator from './app/tabs/tab-navigator/tab-navigator';
+import {NavigationContainer} from '@react-navigation/native';
 
 function App(): JSX.Element {
   return (
-    <SafeAreaView>
-      <StatusBar backgroundColor="#00dba4" />
-      <TabNavigator />
-    </SafeAreaView>
+    <NavigationContainer>
+      <SafeAreaView>
+        <StatusBar backgroundColor="#00dba4" />
+        <TabNavigator />
+      </SafeAreaView>
+    </NavigationContainer>
   );
 }
 
