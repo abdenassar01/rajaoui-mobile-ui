58808ed8c1bbe6b27bdd206caee2db2e1b6b6715
abdenassar <abdenassaramimi99@gmail.com>
2023-08-24 18:55:19 +0100

    adding main components for hero section

--- app/screens/home/blocks/hero.style.ts
+++ app/screens/home/blocks/hero.style.ts
@@ -1,4 +1,15 @@
-import {View} from 'react-native';
+import {ImageBackground, View} from 'react-native';
 import {styled} from 'styled-components';
+import {MainText} from '../../../utils/text';
 
 export const HeroWrapper = styled(View)``;
+export const CardWrapper = styled(ImageBackground)``;
+export const ProfileSection = styled(View)``;
+export const TopSection = styled(View)``;
+export const CenterSection = styled(View)``;
+export const BottomSection = styled(View)``;
+export const NameSection = styled(View)``;
+export const EagleImage = styled(View)``;
+export const NameHeading = styled(MainText)``;
+export const MainTextHeading = styled(MainText)``;
+export const HelperText = styled(MainText)``;
