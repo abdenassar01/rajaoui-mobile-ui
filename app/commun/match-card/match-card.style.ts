d26e20f0553da8e1152f5dbe301a6d3c25de71e5
abdenassar <abdenassaramimi99@gmail.com>
2023-08-31 14:42:49 +0100

    putting dumb data on next match card

--- app/commun/match-card/match-card.style.ts
+++ app/commun/match-card/match-card.style.ts
@@ -1,5 +1,6 @@
-import {ImageBackground} from 'react-native';
+import {Image, ImageBackground, View} from 'react-native';
 import {styled} from 'styled-components';
+import {MainText} from '../../utils/text';
 
 export const MatchCardWrapper = styled(ImageBackground)`
   height: auto;
@@ -7,4 +8,53 @@ export const MatchCardWrapper = styled(ImageBackground)`
   width: 100%;
   border-radius: 10px;
   position: relative;
+`;
+
+export const EventDetails = styled(View)`
+  flex-direction: row;
+  justify-content: space-between;
+  max-width: 35%;
+
+  position: absolute;
+  left: 21.3%;
+  bottom: 9%;
+`;
+
+export const TeamLogo = styled(Image)`
+  width: 35px;
+  height: auto;
+  aspect-ratio: 1/1;
+`;
+
+export const FixtureTime = styled(View)`
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const Time = styled(MainText)`
+  font-size: 10px;
+`;
+
+export const Date = styled(MainText)`
+  font-size: 12px;
+`;
+
+export const MatchTeams = styled(View)`
+  position: absolute;
+  left: 12px;
+  top: 30%;
+`;
+
+export const OpenentsNamesHeading = styled(MainText)`
+  color: white;
+  font-size: 16px;
+`;
+
+export const VersusText = styled(MainText)`
+  color: yellow;
+`;
+
+export const Staduim = styled(MainText)`
+  color: white;
+  font-size: 10px;
 `;
--- app/commun/match-card/match-card.tsx
+++ app/commun/match-card/match-card.tsx
@@ -1,5 +1,16 @@
 import React from 'react';
-import {MatchCardWrapper} from './match-card.style';
+import {
+  Date,
+  EventDetails,
+  FixtureTime,
+  MatchCardWrapper,
+  MatchTeams,
+  OpenentsNamesHeading,
+  Staduim,
+  TeamLogo,
+  Time,
+  VersusText,
+} from './match-card.style';
 
 const MatchCard = () => {
   return (
@@ -7,7 +18,25 @@ const MatchCard = () => {
       // eslint-disable-next-line react-native/no-inline-styles
       imageStyle={{borderRadius: 10}}
       source={require('../../../assets/images/next-match-background.png')}>
-      {/* hello world */}
+      <EventDetails>
+        <TeamLogo
+          source={{uri: 'https://api.sofascore.com/api/v1/team/41757/image'}}
+        />
+        <FixtureTime>
+          <Date>12 Fev - </Date>
+          <Time>12:00</Time>
+        </FixtureTime>
+        <TeamLogo
+          source={{uri: 'https://api.sofascore.com/api/v1/team/41757/image'}}
+        />
+      </EventDetails>
+      <MatchTeams>
+        <OpenentsNamesHeading>
+          Raja CA
+          <VersusText> VS </VersusText>AS FAR
+        </OpenentsNamesHeading>
+        <Staduim>Complexe Mohamed V</Staduim>
+      </MatchTeams>
     </MatchCardWrapper>
   );
 };
