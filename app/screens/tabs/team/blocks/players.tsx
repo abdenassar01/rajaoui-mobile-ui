9b5e9e6f14665b097a2dcae7792d5db4a1215123
abdenassar <abdenassaramimi99@gmail.com>
2023-09-16 11:45:49 +0100

    fix navigations errors

--- app/screens/tabs/team/blocks/players.tsx
+++ app/screens/tabs/team/blocks/players.tsx
@@ -6,20 +6,23 @@ import {ThemeType} from '../../../../utils/theme';
 import {useQuery} from '@tanstack/react-query';
 import {teamPlayers} from '../../../../../resources';
 import {PlayerType} from '../../../../../types/player';
+import {useNavigation} from '@react-navigation/native';
+import {StackNavigationProp} from '@react-navigation/stack';
 
 type Props = {
   theme: ThemeType;
-  navigation: any;
 };
 
-const Players = ({theme, navigation}: Props) => {
+const Players = ({theme}: Props) => {
   const {data: players, isLoading} = useQuery<PlayerType[]>(
     ['fetching team players'],
     teamPlayers,
   );
 
+  const navigation = useNavigation<StackNavigationProp<any>>();
+
   return (
-    <TabItem onPress={() => navigation.navigate('Players')}>
+    <TabItem onPress={() => navigation.navigate('Player')}>
       <TabIcon>
         <Ionicons name="people" size={100} color={theme.primary} />
       </TabIcon>
