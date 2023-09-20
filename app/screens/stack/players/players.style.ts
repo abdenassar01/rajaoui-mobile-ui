5134f4d72672670d17c5f5a13cefd7ca74aee7a2
abdenassar <abdenassaramimi99@gmail.com>
2023-09-20 16:25:35 +0100

    adding starting styles

--- app/screens/stack/players/players.style.ts
+++ app/screens/stack/players/players.style.ts
@@ -3,13 +3,32 @@ import {ScrollView} from 'react-native-gesture-handler';
 import {styled} from 'styled-components';
 import {MainText} from '../../../utils/text';
 
-export const PlayerListWrapper = styled(ScrollView)``;
+export const PlayerListWrapper = styled(ScrollView)`
+  width: 100%;
+  margin-top: 20px;
+`;
 
-export const PlayerItemWrapper = styled(TouchableOpacity)``;
+export const PlayerItemWrapper = styled(TouchableOpacity)`
+  width: 100%;
+  padding: 0 10px;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  background-color: ${({theme}) => theme.secondaryBackground};
+  border-bottom-width: 1px;
+  border-bottom-color: ${({theme}) => theme.primary};
+`;
 
-export const PlayerAvatar = styled(Image)``;
+export const PlayerAvatar = styled(Image)`
+  width: 50px;
+  height: 50px;
+`;
 
-export const PlayerInfos = styled(View)``;
+export const PlayerInfos = styled(View)`
+  flex-direction: row;
+  align-items: center;
+  gap: 15px;
+`;
 
 export const PlayerName = styled(MainText)``;
 
