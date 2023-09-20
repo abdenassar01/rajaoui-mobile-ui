7808192232e3789764b59456cf50fd61cbe36b2d
abdenassar <abdenassaramimi99@gmail.com>
2023-09-20 15:35:54 +0100

    adding boilerplate for player item

--- app/screens/stack/players/players.style.ts
+++ app/screens/stack/players/players.style.ts
@@ -1,4 +1,20 @@
+import {TouchableOpacity, View} from 'react-native';
 import {ScrollView} from 'react-native-gesture-handler';
 import {styled} from 'styled-components';
+import {MainText} from '../../../utils/text';
 
 export const PlayerListWrapper = styled(ScrollView)``;
+
+export const PlayerItemWrapper = styled(TouchableOpacity)``;
+
+export const PlayerInfos = styled(View)``;
+
+export const PlayerName = styled(MainText)``;
+
+export const PlayerJerseyNumber = styled(MainText)``;
+
+export const PlayerNationalityWrapper = styled(View)``;
+
+export const PlayerNationalityText = styled(MainText)``;
+
+export const PlayerNationalityFlag = styled(View)``;
